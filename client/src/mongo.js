9662b16cda27ead29f379bce3353491089f8e034
ChrisJVoss <chrisvoss92@gmail.com>
2017-11-09 13:39:56 -0800

    Set up mongdb schema using environment variables to hide db URL.

---
+++ client/src/mongo.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DB_URL)
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+
+})
+
+const Schema = mongoose.Schema
+
+const vehicleSchema = new Schema({
+  Make: String,
+  Model: String,
+  Year: String,
+  VIN: String,
+  Engine: String,
+  Transmission: String,
+  Images: String,
+  Description: String
+})
