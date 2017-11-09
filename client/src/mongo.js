0f8fd9eb8cd1ff6da5d9d858e10b37721f853d67
ChrisJVoss <chrisvoss92@gmail.com>
2017-11-09 13:45:31 -0800

    Added callback to indicate successfull connection.

--- client/src/mongo.js
+++ client/src/mongo.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DB_URL)
+mongoose.connect(process.env.DB_URL,() => {
+  console.log('connected!')
+})
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
