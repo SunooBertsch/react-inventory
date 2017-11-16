f7ccff2a02e3f01c82e9b1d1af487edba881dacb
ChrisJVoss <chrisvoss92@gmail.com>
2017-11-15 16:13:44 -0800

    Added placeholders and unique names to input fields.

--- client/public/main.js
+++ client/public/main.js
@@ -5,14 +5,13 @@ class SubmitNewCar extends React.Component {
   render() {
     return (
       <form action="/cars" method="POST">
-        <input type="text" placeholder="" name="">
-        <input type="text" placeholder="" name="">
-        <input type="text" placeholder="" name="">
-        <input type="text" placeholder="" name="">
-        <input type="text" placeholder="" name="">
-        <input type="text" placeholder="" name="">
-        <input type="text" placeholder="" name="">
-        <input type="text" placeholder="" name="">
+        <input type="text" placeholder="Make" name="Make">
+        <input type="text" placeholder="Model" name="Model">
+        <input type="text" placeholder="Year" name="Year">
+        <input type="text" placeholder="VIN" name="VIN">
+        <input type="text" placeholder="Engine" name="Engine">
+        <input type="text" placeholder="Transmission" name="Transmission">
+        <input type="text" placeholder="Description" name="Description">
     )
   }
 }
