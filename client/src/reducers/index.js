9fc08c811b16fccd8382f797382d86b51ea0949f
SunooBertsch <sbertsch92@yahoo.com>
2017-11-29 11:22:55 -0800

    additional redux setup

---
+++ client/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { routerReducer } from "react-router-redux";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  routerReducer
+});
+
+export default rootReducer;
