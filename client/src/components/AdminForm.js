914773ab5b7caa9149fbc4aa73b36090c58c6405
SunooBertsch <sbertsch92@yahoo.com>
2017-12-04 10:44:26 -0800

    dropzone image uploader added to admin form

--- client/src/components/AdminForm.js
+++ client/src/components/AdminForm.js
@@ -1,66 +1,110 @@
-import React from "react";
+import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
+import ImgUploader from "react-dropzone";
 
-const AdminPortal = props => {
-  const { handleSubmit, pristine, rest, submitting } = props;
+const FILE_FIELD_NAME = "files";
 
+const renderImgUploader = field => {
+  const files = field.input.value;
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Make</label>
-        <Field name="make" component="input" type="text" placeholder="Make" />
-      </div>
-      <div>
-        <label>Model</label>
-        <Field name="model" component="input" type="text" placeholder="Model" />
-      </div>
-      <div>
-        <label>Transmission</label>
-        <Field
-          name="transmission"
-          component="input"
-          type="text"
-          placeholder="Transmission"
-        />
-      </div>
-      <div>
-        <label>Mileage</label>
-        <Field
-          name="mileage"
-          component="input"
-          type="number"
-          placeholder="Mileage"
-        />
-      </div>
-      <div>
-        <label>Engine</label>
-        <Field
-          name="engine"
-          component="input"
-          type="text"
-          placeholder="Engine"
-        />
-      </div>
-      <div>
-        <label>Trim Level</label>
-        <Field
-          name="trimLevel"
-          component="input"
-          type="text"
-          placeholder="Trim Level"
-        />
-      </div>
-      <div>
-        <label>Year</label>
-        <Field name="year" component="input" type="number" placeholder="Year" />
-      </div>
-      <div>
-        <button type="submit" disabled={pristine || submitting}>
-          Submit
-        </button>
-      </div>
-    </form>
+    <div>
+      <ImgUploader
+        name={field.name}
+        onDrop={(files, e) => field.input.onChange(files)}>
+        <div>ImgUploader</div>
+      </ImgUploader>
+    </div>
   );
 };
+
+class AdminPortal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      files: []
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit() {
+    console.log("submitted");
+  }
+
+  render() {
+    const { handleSubmit, pristine, rest, submitting } = this.props;
+    return (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Make</label>
+          <Field name="make" component="input" type="text" placeholder="Make" />
+        </div>
+        <div>
+          <label>Model</label>
+          <Field
+            name="model"
+            component="input"
+            type="text"
+            placeholder="Model"
+          />
+        </div>
+        <div>
+          <label>Transmission</label>
+          <Field
+            name="transmission"
+            component="input"
+            type="text"
+            placeholder="Transmission"
+          />
+        </div>
+        <div>
+          <label>Mileage</label>
+          <Field
+            name="mileage"
+            component="input"
+            type="number"
+            placeholder="Mileage"
+          />
+        </div>
+        <div>
+          <label>Engine</label>
+          <Field
+            name="engine"
+            component="input"
+            type="text"
+            placeholder="Engine"
+          />
+        </div>
+        <div>
+          <label>Trim Level</label>
+          <Field
+            name="trimLevel"
+            component="input"
+            type="text"
+            placeholder="Trim Level"
+          />
+        </div>
+        <div>
+          <label>Year</label>
+          <Field
+            name="year"
+            component="input"
+            type="number"
+            placeholder="Year"
+          />
+        </div>
+        <div>
+          <Field name={FILE_FIELD_NAME} component={renderImgUploader} />
+        </div>
+        <div>
+          <button
+            onClick={this.props.handle}
+            type="submit"
+            disabled={pristine || submitting}>
+            Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
 
 export default reduxForm({ form: "car" })(AdminPortal);
