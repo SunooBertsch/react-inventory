332bbe026bac74b7a6624f7a9c8da96235e35cb6
ChrisJVoss <chrisvoss92@gmail.com>
2017-12-05 09:07:18 -0800

    Modal now displays carousel.

--- client/src/components/Inventory.js
+++ client/src/components/Inventory.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import {Card, CardTitle, Button, Modal, Carousel, Slider, Slide} from 'react-materialize'
 
-
+const imageList = [
+  'http://hanabi.autoweek.com/sites/default/files/styles/gen-1200-675/public/honda_civic_si_prototype-06.jpg?itok=aGZD_lya',
+  'https://media.ed.edmunds-media.com/chevrolet/cobalt/2005/oem/2005_chevrolet_cobalt_coupe_ss-supercharged_fq_oem_1_500.jpg',
+  'http://www.tflcar.com/wp-content/uploads/2017/03/Porsche-718-Cayman-LSD.jpg',
+  'http://www.dupontregistry.com/autos/virtual_folder/vehicle_photos/1e03db71-d6e2-4068-a8a1-277683be61a5/4db21cdf-c04e-458c-abe9-a2ff187ddf45/Shelby_GT350R.jpg?404=default&mode=pad,pad&width=100%&height=100%'
+]
 class Inventory extends React.Component {
   constructor() {
     super()
@@ -23,30 +28,11 @@ class Inventory extends React.Component {
                     <Modal
                       header='Modal Header'
                       trigger={<Button>More</Button>}>
-                      <Slider interval={20000} fullscreen={false}>
-                        <Slide
-                          src="http://hanabi.autoweek.com/sites/default/files/styles/gen-1200-675/public/honda_civic_si_prototype-06.jpg?itok=aGZD_lya">
-                        </Slide>
-                        <Slide
-                          src="https://media.ed.edmunds-media.com/chevrolet/cobalt/2005/oem/2005_chevrolet_cobalt_coupe_ss-supercharged_fq_oem_1_500.jpg">
-                        </Slide>
-                        <Slide
-                          src="http://www.tflcar.com/wp-content/uploads/2017/03/Porsche-718-Cayman-LSD.jpg">
-                        </Slide>
-                        <Slide
-                          src="http://www.dupontregistry.com/autos/virtual_folder/vehicle_photos/1e03db71-d6e2-4068-a8a1-277683be61a5/4db21cdf-c04e-458c-abe9-a2ff187ddf45/Shelby_GT350R.jpg?404=default&mode=pad,pad&width=100%&height=100%">
-                        </Slide>
-                      </Slider>
+                      <Carousel style={{height: '400px'}} options={{ fullWidth: true}} images={imageList} />
                     </Modal>
                   ]}>
                   Additional info here.
                 </Card>
-                <Carousel options={{ fullWidth: true}} images={[
-                  'http://hanabi.autoweek.com/sites/default/files/styles/gen-1200-675/public/honda_civic_si_prototype-06.jpg?itok=aGZD_lya',
-                  'https://media.ed.edmunds-media.com/chevrolet/cobalt/2005/oem/2005_chevrolet_cobalt_coupe_ss-supercharged_fq_oem_1_500.jpg',
-                  'http://www.tflcar.com/wp-content/uploads/2017/03/Porsche-718-Cayman-LSD.jpg',
-                  'http://www.dupontregistry.com/autos/virtual_folder/vehicle_photos/1e03db71-d6e2-4068-a8a1-277683be61a5/4db21cdf-c04e-458c-abe9-a2ff187ddf45/Shelby_GT350R.jpg?404=default&mode=pad,pad&width=100%&height=100%'
-                ]} />
               </div>
           )
         })
@@ -68,3 +54,20 @@ class Inventory extends React.Component {
 }
 
 export default Inventory
+
+/*
+<Slider interval={20000} fullscreen={false}>
+  <Slide
+    src="http://hanabi.autoweek.com/sites/default/files/styles/gen-1200-675/public/honda_civic_si_prototype-06.jpg?itok=aGZD_lya">
+  </Slide>
+  <Slide
+    src="https://media.ed.edmunds-media.com/chevrolet/cobalt/2005/oem/2005_chevrolet_cobalt_coupe_ss-supercharged_fq_oem_1_500.jpg">
+  </Slide>
+  <Slide
+    src="http://www.tflcar.com/wp-content/uploads/2017/03/Porsche-718-Cayman-LSD.jpg">
+  </Slide>
+  <Slide
+    src="http://www.dupontregistry.com/autos/virtual_folder/vehicle_photos/1e03db71-d6e2-4068-a8a1-277683be61a5/4db21cdf-c04e-458c-abe9-a2ff187ddf45/Shelby_GT350R.jpg?404=default&mode=pad,pad&width=100%&height=100%">
+  </Slide>
+</Slider>
+*/
