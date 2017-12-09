0c03d31d6e9e4dc3c46eb029b68090ff8850a267
ChrisJVoss <chrisvoss92@gmail.com>
2017-12-08 18:28:14 -0800

    Selecting a car now displays carousel and additional information.

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -11,40 +11,41 @@ const imageList = [
 class Card extends React.Component {
   constructor(props) {
     super(props)
-    console.log(this.props.id)
+    this.state = {}
   }
-  conponentDidMount() {
+  componentDidMount() {
     fetch('/cars/' + this.props.id)
       .then(res => {
         return res.json()
       })
       .then(card => {
         let cardPage = card.map((info) => {
-          console.log(info)
           return (
+          <div className="carousel-container" key={info.model}>
             <Carousel>
               <Carousel.Item>
-                <img width={900} height={500} alt="900x500" src="/assets/carousel.png" />
+                <img width={900} height={500} alt="900x500" src="http://hanabi.autoweek.com/sites/default/files/styles/gen-1200-675/public/honda_civic_si_prototype-06.jpg?itok=aGZD_lya" />
                 <Carousel.Caption>
                   <h3>First slide label</h3>
                   <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
-                <img width={900} height={500} alt="900x500" src="/assets/carousel.png" />
+                <img width={900} height={500} alt="900x500" src="http://www.tflcar.com/wp-content/uploads/2017/03/Porsche-718-Cayman-LSD.jpg" />
                 <Carousel.Caption>
                   <h3>Second slide label</h3>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
-                <img width={900} height={500} alt="900x500" src="/assets/carousel.png" />
+                <img width={900} height={500} alt="900x500" src="http://www.dupontregistry.com/autos/virtual_folder/vehicle_photos/1e03db71-d6e2-4068-a8a1-277683be61a5/4db21cdf-c04e-458c-abe9-a2ff187ddf45/Shelby_GT350R.jpg?404=default&mode=pad,pad&width=100%&height=100%" />
                 <Carousel.Caption>
                   <h3>Third slide label</h3>
                   <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                 </Carousel.Caption>
               </Carousel.Item>
             </Carousel>
+          </div>
           )
         })
         this.setState({ cardPage })
--- client/src/components/Inventory.js
+++ client/src/components/Inventory.js
@@ -18,7 +18,6 @@ class Inventory extends React.Component {
       showCard: true,
       carId: id
     })
-    console.log(this.state.showCard, this.state.carId)
   }
 
   componentDidMount() {
@@ -29,19 +28,18 @@ class Inventory extends React.Component {
       .then(cars => {
         this.setState({ cars })
         let carStats = cars.map((stats) => {
-          //let uniqueId = '/cars/' + (stats._id).toString()
           return (
-              <Col key={stats._id} xs={12} md={6} lg={4}>
-                <Thumbnail src='https://i.ytimg.com/vi/0Q79esFqfOE/maxresdefault.jpg' alt="242x200">
-                  <h3>{stats.year} {stats.make} {stats.model} {stats.trimLevel}</h3>
-                  <p>Description</p>
-                  <div>
-                    <Button onClick={this.handleClick(stats._id)}>
-                      More
-                    </Button>
-                  </div>
-                </Thumbnail>
-              </Col>
+            <Col key={stats._id} xs={12} md={6} lg={4}>
+              <Thumbnail src='https://i.ytimg.com/vi/0Q79esFqfOE/maxresdefault.jpg' alt="242x200">
+                <h3>{stats.year} {stats.make} {stats.model} {stats.trimLevel}</h3>
+                <p>Description</p>
+                <div>
+                  <Button onClick={() => this.handleClick(stats._id)}>
+                    More
+                  </Button>
+                </div>
+              </Thumbnail>
+            </Col>
           )
         })
         this.setState({ carStats })
@@ -55,9 +53,9 @@ class Inventory extends React.Component {
   }
 
   render() {
-    /*if(this.state.showCard){
+    if(this.state.showCard){
       return this.renderCard();
-    }*/
+    }
     return (
       <div className="inventory">
         <h1>Inventory</h1>
