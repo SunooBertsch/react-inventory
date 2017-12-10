c66164718e440b12ecacaf999b29ffa53f3e5b18
ChrisJVoss <chrisvoss92@gmail.com>
2017-12-09 16:08:05 -0800

    Added image list to mongo.js to act as local DB and deleted bootsrap carousel from VehicleCard.js.

--- client/src/components/VehicleCard.js
+++ client/src/components/VehicleCard.js
@@ -1,13 +1,5 @@
 import React from 'react'
-import { Carousel, Grid, Row, Col } from 'react-bootstrap'
-/*
-const imageList = [
-  'http://hanabi.autoweek.com/sites/default/files/styles/gen-1200-675/public/honda_civic_si_prototype-06.jpg?itok=aGZD_lya',
-  'https://media.ed.edmunds-media.com/chevrolet/cobalt/2005/oem/2005_chevrolet_cobalt_coupe_ss-supercharged_fq_oem_1_500.jpg',
-  'http://www.tflcar.com/wp-content/uploads/2017/03/Porsche-718-Cayman-LSD.jpg',
-  'http://www.dupontregistry.com/autos/virtual_folder/vehicle_photos/1e03db71-d6e2-4068-a8a1-277683be61a5/4db21cdf-c04e-458c-abe9-a2ff187ddf45/Shelby_GT350R.jpg?404=default&mode=pad,pad&width=100%&height=100%'
-]
-*/
+import images from '../mongo.js'
 class Card extends React.Component {
   constructor(props) {
     super(props)
@@ -21,31 +13,6 @@ class Card extends React.Component {
       .then(card => {
         let cardPage = card.map((info) => {
           return (
-          <div className="carousel-container" key={info.model}>
-            <Carousel>
-              <Carousel.Item>
-                <img width={900} height={500} alt="900x500" src="http://hanabi.autoweek.com/sites/default/files/styles/gen-1200-675/public/honda_civic_si_prototype-06.jpg?itok=aGZD_lya" />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img width={900} height={500} alt="900x500" src="http://www.tflcar.com/wp-content/uploads/2017/03/Porsche-718-Cayman-LSD.jpg" />
-                <Carousel.Caption>
-                  <h3>Second slide label</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img width={900} height={500} alt="900x500" src="http://www.dupontregistry.com/autos/virtual_folder/vehicle_photos/1e03db71-d6e2-4068-a8a1-277683be61a5/4db21cdf-c04e-458c-abe9-a2ff187ddf45/Shelby_GT350R.jpg?404=default&mode=pad,pad&width=100%&height=100%" />
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
-          </div>
           )
         })
         this.setState({ cardPage })
--- client/src/mongo.js
+++ client/src/mongo.js
@@ -1,25 +1,9 @@
-require('dotenv').config()
+export const images = [
+  "http://hanabi.autoweek.com/sites/default/files/styles/gen-1200-675/public/02-dinan-m235i-s3.jpg?itok=y6QOJLvb",
+  "http://hanabi.autoweek.com/sites/default/files/styles/gen-1200-675/public/04-dinan-m235i-s3.jpg?itok=yRFwo8s0",
+  "https://i.kinja-img.com/gawker-media/image/upload/s--7mji7YfI--/c_scale,fl_progressive,q_80,w_800/pldkkoce512enloh4tbc.jpg",
+  "http://speedhunters-wp-production.s3.amazonaws.com/wp-content/uploads/2015/12/23131611/Larry_Chen_Speedhunters_Dinan_M2_011.jpg",
+  "https://s.aolcdn.com/hss/storage/midas/b206bec793898f91ba270772407e4895/202659636/lead6-dinan-m235i-fd.jpg",
+  "https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/916x515/quality/95/http://www.blogcdn.com/slideshows/images/slides/362/816/6/S3628166/slug/l/03-dinan-m235i-fd-1.jpg"
+];
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DB_URL,() => {
-  console.log('connected!')
-})
-
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', () => {
-
-})
-
-const Schema = mongoose.Schema
-
-const vehicleSchema = new Schema({
-  Make: String,
-  Model: String,
-  Year: String,
-  VIN: String,
-  Engine: String,
-  Transmission: String,
-  Images: String,
-  Description: String
-})
