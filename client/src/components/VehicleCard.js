f96960ab4f24e26ef3a8cd5207c87036dcda970c
ChrisJVoss <chrisvoss92@gmail.com>
2017-12-12 17:09:30 -0800

    Vehicle info is now displayed below carousel.

--- client/src/components/VehicleCard.js
+++ client/src/components/VehicleCard.js
@@ -15,11 +15,42 @@ class Card extends React.Component {
           return image.base64;
         });
         return (
-          <Carousel
-            options={{ fullWidth: true, indicators: true }}
-            images={imageList}
-            style={{ height: "auto" }}
-          />
+          <div>
+            <Carousel
+              options={{ fullWidth: true, indicators: true }}
+              images={imageList}
+              style={{ height: "auto" }}
+            />
+            <div className="carInfo">
+              <ul className="collection with-header">
+                <li className="collection-header">
+                  <h5>Price: {info.price}</h5>
+                </li>
+                <li className="collection-item">
+                  {info.year} {info.make} {info.model} {info.trimLevel}
+                </li>
+                <li className="collection-item">
+                  <strong>Mileage:</strong> {info.mileage}
+                </li>
+                <li className="collection-item">
+                  <strong>Engine:</strong> {info.engine}
+                </li>
+                <li className="collection-item">
+                  <strong>Transmission:</strong> {info.transmission}
+                </li>
+                <li className="collection-item">
+                  <strong>Seller Notes:</strong> Lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                  ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                  ea commodo consequat. Duis aute irure dolor in reprehenderit
+                  in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                  in culpa qui officia deserunt mollit anim id est laborum.
+                </li>
+              </ul>
+            </div>
+          </div>
         );
       });
       this.setState({ cardPage });
