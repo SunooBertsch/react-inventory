8014c48acc7a08a20081ad67a80231d1babba5f8
ChrisJVoss <chrisvoss92@gmail.com>
2017-12-13 16:51:39 -0800

    Renamed mongo.js to LandingImages and added banner images.

---
+++ client/src/LandingImages.js
@@ -0,0 +1,8 @@
+const imageList = [
+  "https://i.imgur.com/do9tUO1.jpg",
+  "https://i.imgur.com/FWbhaPM.jpg",
+  "https://i.imgur.com/in8VMMN.jpg",
+  "https://i.imgur.com/Jw2MTvG.jpg",
+  "https://i.imgur.com/ZYnR7Ob.jpg"
+];
+export default imageList;
