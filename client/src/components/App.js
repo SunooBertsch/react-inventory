707c025ccf93ad8a49387ea89035200f0915ca59
ChrisJVoss <chrisvoss92@gmail.com>
2017-12-13 16:50:46 -0800

    Changed header color and width.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -17,7 +17,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container">
+      <div className="container" style={{ width: "95%" }}>
         <BrowserRouter>
           <div>
             <Header />
