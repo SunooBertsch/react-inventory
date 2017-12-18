8c64823c871d94a13a4ddeb108cd733064e172fa
ChrisJVoss <chrisvoss92@gmail.com>
2017-12-17 22:27:49 -0800

    Removed links to other components to avoid errors regarding css frameworks.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,8 +4,8 @@ import { connect } from "react-redux";
 import * as actions from "../actions";
 import Landing from "./Landing";
 import Admin from "./Admin";
-import Inventory from "./Inventory";
-import VehicleCard from "./VehicleCard";
+//import Inventory from "./Inventory";
+//import VehicleCard from "./VehicleCard";
 
 class App extends Component {
   componentDidMount() {
@@ -20,8 +20,6 @@ class App extends Component {
           <div>
             <Route exact path="/" component={Landing} />
             <Route exact path="/admin" component={Admin} />
-            <Route exact path="/inventory" component={Inventory} />
-            <Route path="/inventory/:id" component={VehicleCard} />
           </div>
         </BrowserRouter>
       </div>
