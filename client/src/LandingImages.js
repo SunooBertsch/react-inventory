d05b3025e69f19dd4497cf1f36bbae7c52cc626d
ChrisJVoss <chrisvoss92@gmail.com>
2017-12-17 22:27:10 -0800

    Changed all the images for the landing page carousel.

--- client/src/LandingImages.js
+++ client/src/LandingImages.js
@@ -1,8 +1,9 @@
 const imageList = [
-  "https://i.imgur.com/do9tUO1.jpg",
-  "https://i.imgur.com/FWbhaPM.jpg",
-  "https://i.imgur.com/in8VMMN.jpg",
-  "https://i.imgur.com/Jw2MTvG.jpg",
-  "https://i.imgur.com/ZYnR7Ob.jpg"
+  "https://i.imgur.com/LG4DBiG.jpg",
+  "https://i.imgur.com/cu1Iz0Z.jpg",
+  "https://i.imgur.com/AyqMuvS.jpg",
+  "https://i.imgur.com/hLFt1XR.jpg",
+  "https://i.imgur.com/T7vnw3v.jpg",
+  "https://i.imgur.com/RPEg0wN.jpg"
 ];
 export default imageList;
