43de85654e8dce22f98299d1a2ef138953718554
ChrisJVoss <chrisvoss92@gmail.com>
2018-01-11 18:57:19 -0800

    Set up email address and password for production.

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,7 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  email: process.env.EMAIL,
+  emailPassword: process.env.EMAIL_PASSWORD
 };
