4bfa7e077c4d051376a7c02f290d867fb05f528a
ChrisJVoss <chrisvoss92@gmail.com>
2018-01-15 23:52:34 -0800

    Added routes to handle vehicle specific nodemailer.

--- routes/emailRoute.js
+++ routes/emailRoute.js
@@ -30,8 +30,53 @@ module.exports = app => {
 
     let mailOptions = {
       from: "FastTurtleMotors <fastturtlemotors@gmail.com>",
-      to: "info@fastturtlemotors.com",
+      to: "chrisvoss92@gmail.com",
       subject: "Customer Contact Info",
+      html: output // html body
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log("Message sent: %s", info.messageId);
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+      res.redirect("..");
+    });
+  });
+
+  app.post("/send-car", (req, res) => {
+    console.log(req.body);
+    const output = `
+    <p>You have a new customer!</p>
+    <h3>Contact Details<h3>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+      <li>Vehicle: ${req.body.car}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+    `;
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: keys.email,
+        pass: keys.emailPassword
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
+
+    let mailOptions = {
+      from: "FastTurtleMotors <fastturtlemotors@gmail.com>",
+      to: "chrisvoss92@gmail.com",
+      subject: "Vehicle Specific Email",
       html: output // html body
     };
 
