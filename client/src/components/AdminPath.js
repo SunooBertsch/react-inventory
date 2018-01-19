76e711c951e10954a2afd22e8406446f703da6b5
ChrisJVoss <chrisvoss92@gmail.com>
2018-01-19 15:06:06 -0800

    Admin path.

---
+++ client/src/components/AdminPath.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Admin from "./Admin";
+
+const AdminPath = () => {
+  return (
+    <Route
+      exact
+      path="/u0eh3289qudineu9dhj29uqidhandiu2ejan"
+      component={Admin}
+    />
+  );
+};
+export default AdminPath;
+
