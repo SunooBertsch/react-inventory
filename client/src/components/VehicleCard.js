196992a207924835462cdd4df1ac77ccb14142c7
ChrisJVoss <chrisvoss92@gmail.com>
2018-01-23 20:19:25 -0800

    Replaced all instances of the footer with the Footer component.

--- client/src/components/VehicleCard.js
+++ client/src/components/VehicleCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 import { Carousel } from "react-bootstrap";
 
 class Card extends React.Component {
@@ -141,38 +142,7 @@ class Card extends React.Component {
                 </button>
               </form>
             </div>
-            <div
-              className="bottomInfo"
-              style={{
-                borderTop: "1px solid grey",
-                paddingBottom: "10px",
-                backgroundColor: "#e7e7e7",
-                color: "#213159"
-              }}
-            >
-              <div className="row" style={{ marginLeft: "15px" }}>
-                <div className="col-sm-4" style={{}}>
-                  <h4>About Us</h4>
-                </div>
-                <div className="col-sm-4" style={{}}>
-                  <h4>Contact Us</h4>
-                  <ul className="list-unstyled">
-                    <li>Phone: (714) 987-1661</li>
-                    <li>Email: info@fastturtlemotors.com</li>
-                  </ul>
-                </div>
-                <div className="col-sm-4" style={{}}>
-                  <h4>Follow Us</h4>
-                  <a href="https://www.facebook.com/FastTurtleMotors/">
-                    <i
-                      className="fa fa-facebook"
-                      aria-hidden="true"
-                      style={{ color: "#213159" }}
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
+            <Footer />
           </div>
         );
       });
