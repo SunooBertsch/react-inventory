6017b7b3f367dccaa2d556fac829ebb44b70dd9b
SunooBertsch <sbertsch92@yahoo.com>
2018-01-29 17:15:40 -0800

    auction card styling

--- client/src/components/AuctionCard.js
+++ client/src/components/AuctionCard.js
@@ -53,89 +53,112 @@ class AuctionCard extends React.Component {
     });
     this.makeCarousel(imageList);
     return (
-      <div
-        style={{
-          color: "#e7e7e7"
-        }}>
-        <div className="carousel-and-details  align-middle">
+      <div>
+        <div className="row" style={{ height: "500px" }}>
           <div
-            id="carouselVehicleCard"
-            className="carousel slide"
-            data-ride="carousel">
-            <ol className="carousel-indicators">
-              <li
-                data-target="#carouselVehicleCard"
-                data-slide-to="0"
-                className="active"
-              />
-              <li data-target="#carouselVehicleCard" data-slide-to="1" />
-              <li data-target="#carouselVehicleCard" data-slide-to="2" />
-            </ol>
-            <div className="carousel-inner">{this.state.slides}</div>
-            <a
-              className="carousel-control-prev"
-              href="#carouselVehicleCard"
-              role="button"
-              data-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true" />
-              <span className="sr-only">Previous</span>
-            </a>
-            <a
-              className="carousel-control-next"
-              href="#carouselVehicleCard"
-              role="button"
-              data-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true" />
-              <span className="sr-only">Next</span>
-            </a>
-          </div>
-          <div className="col-xs-1 col-sm-0" />
-          <div
-            className="align-middle col-xs-12"
             style={{
-              backgroundColor: "#e7e7e7",
-              color: "#912525",
-              height: "100%",
               width: "100%",
-              paddingTop: "20px"
-            }}>
-            <h4 style={{ textAlign: "center" }}>
-              {card.year} {card.make} {card.model} {card.trimLevel}{" "}
-            </h4>
-            <ul className="collection with-header" style={{ padding: "5%" }}>
-              <li className="collection-header">
-                <strong>Price:</strong>{" "}
-                {card.sold ? "SOLD!" : numberWithCommas(card.price)}
-              </li>
-              <li className="collection-item">
-                <strong>Year:</strong> {card.year}
-              </li>
-              <li className="collection-item">
-                <strong>Make:</strong> {card.make}
-              </li>
-              <li className="collection-item">
-                <strong>Model:</strong> {card.model}
-              </li>
-              <li className="collection-item">
-                <strong>Trim:</strong> {card.trimLevel}
-              </li>
-              <li className="collection-item">
-                <strong>Mileage:</strong> {card.mileage}
-              </li>
-              <li className="collection-item">
-                <strong>Engine:</strong> {card.engine}
-              </li>
-              <li className="collection-item">
-                <strong>Transmission:</strong> {card.transmission}
-              </li>
-              <li className="collection-item">
-                <strong>Seller Notes:</strong> {card.sellersNotes}
-              </li>
-            </ul>
+              height: "auto",
+              margin: "20px"
+            }}
+            className="col-sm-5">
+            <div
+              style={{ width: "100%", margin: "auto" }}
+              id="carouselVehicleCard"
+              className="carousel slide"
+              data-ride="carousel">
+              <ol className="carousel-indicators">
+                <li
+                  data-target="#carouselVehicleCard"
+                  data-slide-to="0"
+                  className="active"
+                />
+                <li data-target="#carouselVehicleCard" data-slide-to="1" />
+                <li data-target="#carouselVehicleCard" data-slide-to="2" />
+              </ol>
+              <div className="carousel-inner">{this.state.slides}</div>
+              <a
+                className="carousel-control-prev"
+                href="#carouselVehicleCard"
+                role="button"
+                data-slide="prev">
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                />
+                <span className="sr-only">Previous</span>
+              </a>
+              <a
+                className="carousel-control-next"
+                href="#carouselVehicleCard"
+                role="button"
+                data-slide="next">
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                />
+                <span className="sr-only">Next</span>
+              </a>
+            </div>
           </div>
           <div
-            className="col-xs-offset-1 col-xs-10"
-            style={{ backgroundColor: "#213461", marginTop: "30px" }}>
+            style={{
+              width: "auto",
+              height: "100%",
+              margin: "auto",
+              "box-shadow": "8px 8px 25px lightGrey"
+            }}
+            className="col-sm-6 card">
+            <div
+              className="card-block"
+              style={{
+                color: "#213461",
+                height: "100%",
+                width: "100%",
+                margin: "auto"
+              }}>
+              <h4 className="card-title" style={{ paddingTop: "20px" }}>
+                {card.year} {card.make} {card.model} {card.trimLevel}{" "}
+              </h4>
+              <ul className="list-group" style={{ fontSize: "10px" }}>
+                <li className="list-group-item">
+                  <strong>Price:</strong> {card.sold ? "SOLD!" : card.price}
+                </li>
+                <li className="list-group-item">
+                  <strong>Year:</strong> {card.year}
+                </li>
+                <li className="list-group-item">
+                  <strong>Make:</strong> {card.make}
+                </li>
+                <li className="list-group-item">
+                  <strong>Model:</strong> {card.model}
+                </li>
+                <li className="list-group-item">
+                  <strong>Trim:</strong> {card.trimLevel}
+                </li>
+                <li className="list-group-item">
+                  <strong>Mileage:</strong> {card.mileage}
+                </li>
+                <li className="list-group-item">
+                  <strong>Engine:</strong> {card.engine}
+                </li>
+                <li className="list-group-item">
+                  <strong>Transmission:</strong> {card.transmission}
+                </li>
+                <li className="list-group-item">
+                  <strong>Seller Notes:</strong> {card.sellersNotes}
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div
+            className="offset-sm-2 col-sm-8 card"
+            style={{
+              color: "#213461",
+              marginTop: "30px",
+              padding: "20px",
+              "box-shadow": "8px 8px 25px lightGrey"
+            }}>
             <h3>Ask us about this car!</h3>
             <form method="POST" action="send-car">
               <div className="form-group">
@@ -143,7 +166,7 @@ class AuctionCard extends React.Component {
                   className="form-control"
                   type="text"
                   name="name"
-                  placeHolder="Name"
+                  placeholder="Name"
                 />
               </div>
               <div className="form-group">
@@ -151,7 +174,7 @@ class AuctionCard extends React.Component {
                   className="form-control"
                   type="email"
                   name="email"
-                  placeHolder="Email"
+                  placeholder="Email"
                 />
               </div>
               <div className="form-group">
@@ -159,7 +182,7 @@ class AuctionCard extends React.Component {
                   className="form-control"
                   type="text"
                   name="phone"
-                  placeHolder="Phone"
+                  placeholder="Phone"
                 />
               </div>
               <div className="form-group">
@@ -167,7 +190,7 @@ class AuctionCard extends React.Component {
                   className="form-control"
                   name="message"
                   rows="5"
-                  placeHolder="Message"
+                  placeholder="Message"
                 />
               </div>
               <div className="form-group">
@@ -178,7 +201,7 @@ class AuctionCard extends React.Component {
                   value={carName}
                 />
               </div>
-              <button className="btn submit-email" type="submit">
+              <button className="btn btn-success submit-email" type="submit">
                 Submit
               </button>
             </form>
@@ -190,8 +213,16 @@ class AuctionCard extends React.Component {
 
   render() {
     return (
-      <div style={{ width: "100%", paddingTop: "30px" }}>
-        <div>{this.props.inventory.length > 0 ? this.renderPage() : ""}</div>
+      <div
+        style={{
+          padding: "30px",
+          "box-shadow": "0px 2px 12px lightGrey"
+        }}>
+        <div
+          className="container"
+          style={{ height: "auto", "min-height": "200vh" }}>
+          {this.props.inventory.length > 0 ? this.renderPage() : ""}
+        </div>
       </div>
     );
   }
