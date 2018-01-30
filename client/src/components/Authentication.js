07e14881be12a6fdd730cc492fd9fc0687981111
SunooBertsch <sbertsch92@yahoo.com>
2018-01-29 20:46:28 -0800

    minor authentication styling

--- client/src/components/Authentication.js
+++ client/src/components/Authentication.js
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 class Authentication extends Component {
   render() {
     return (
-      <div>
+      <div style={{ height: "70vh",
+      "box-shadow": "0px 2px 12px lightGrey" }}>
         <div style={{ textAlign: "center" }}>
           <div style={{ height: "200px", width: "100%" }} />
           <a className="btn btn-primary" href="/auth/google">
