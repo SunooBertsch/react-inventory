f7c52adaf868524fc191c92ab0aba513ef343b44
ChrisJVoss <chrisvoss92@gmail.com>
2018-02-01 17:08:55 -0800

    Modal-Carousel boilerplate.

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,3 +1,66 @@
 import React from "react";
 
-const Carousel = images => {};
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="modal" tabindex="-1" role="dialog">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div
+              id="carouselExampleControls"
+              className="carousel slide"
+              data-ride="carousel"
+            >
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img className="d-block w-100" src="..." alt="First slide" />
+                </div>
+                <div className="carousel-item">
+                  <img className="d-block w-100" src="..." alt="Second slide" />
+                </div>
+                <div className="carousel-item">
+                  <img className="d-block w-100" src="..." alt="Third slide" />
+                </div>
+              </div>
+              <a
+                className="carousel-control-prev"
+                href="#carouselExampleControls"
+                role="button"
+                data-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                />
+                <span className="sr-only">Previous</span>
+              </a>
+              <a
+                className="carousel-control-next"
+                href="#carouselExampleControls"
+                role="button"
+                data-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                />
+                <span className="sr-only">Next</span>
+              </a>
+            </div>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-dismiss="modal"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
